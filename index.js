b615503d820acc1b21f3699630a1eb70be61f712
ElkaMaclaud <elkaMaclaud@gmail.com>
2024-04-05 20:52:01 +0300

    authMiddleware works correctly

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const router = require("./router/router");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const fetchGoodsAndSaveToMongoDB = require("./controllers/getDataController");
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
